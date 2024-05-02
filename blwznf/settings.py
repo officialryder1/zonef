86e0102ad7cfb5685b83683d6b76c828171befa7
officialryder1~ <kennethvic07@gmail.com>
2024-05-02 06:30:09 -0700

    added postgres

--- blwznf/settings.py
+++ blwznf/settings.py
@@ -89,7 +89,7 @@ DATABASES = {
     }
 }
 
-
+DATABASES['default'] = dj_database_url.parse(config('POSTGRES'))
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
 
